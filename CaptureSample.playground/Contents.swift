baf7dcd437f9dcb29960a143f5c2c89aaa635a38
zaki-latte <ym.8844.flwease.developer@gmail.com>
2025-01-25 18:40:04 +0900

    12-3完了

--- CaptureSample.playground/Contents.swift
+++ CaptureSample.playground/Contents.swift
@@ -27,3 +27,29 @@ import _Concurrency
  print("4回目のm1を実行しました。localCountは\(m1()), globalCountは\(globalCount)です")
  print("2回目のm2を実行しました。localCountは\(m2()), globalCountは\(globalCount)です")
  */
+
+//MARK: - 12.3
+var m1: (() -> ())! = nil
+var m2: (() -> ())! = nil
+
+func makerW(_ a: Int) {
+    var localVar = a
+    m1 = {
+        localVar += 1
+        print("m1: \(localVar)")
+    }
+    m2 = {
+        localVar += 5
+        print("m2: \(localVar)")
+    }
+    
+    m1()
+    print("--: \(localVar)")
+    m2()
+    print("--: \(localVar)")
+}
+
+makerW(10)
+m1()
+m2()
+m1()
